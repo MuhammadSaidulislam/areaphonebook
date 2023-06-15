1b2b7404a4f2df0ab52bdcda0e6892dc1a2652e3
MuhammadSaidulislam <mislam171222@bscse.uiu.ac.bd>
2023-06-16 00:36:57 +0600

    login api

--- src/Admin/Login.js
+++ src/Admin/Login.js
@@ -1,96 +1,102 @@
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import AddService from "./AddService";
-import './Login.css'
+import "./Login.css";
 
 const Login = () => {
-    // React States
-    const [errorMessages, setErrorMessages] = useState({});
-    const [isSubmitted, setIsSubmitted] = useState(false);
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    // User Login info
-    const database = [
-        {
-            username: "admin",
-            password: "admin"
-        },
-        {
-            username: "user2",
-            password: "pass2"
-        }
-    ];
+  // User Login info
+  const database = [
+    {
+      username: "admin",
+      password: "admin",
+    },
+    {
+      username: "user2",
+      password: "pass2",
+    },
+  ];
 
-    const errors = {
-        uname: "invalid username",
-        pass: "invalid password"
-    };
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password",
+  };
 
-    const handleSubmit = (event) => {
-        //Prevent page reload
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
 
-        var { uname, pass } = document.forms[0];
+    var { uname, pass } = document.forms[0];
 
-        // Find user login info
-        const userData = database.find((user) => user.username === uname.value);
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
 
-        // Compare user info
-        if (userData) {
-            if (userData.password !== pass.value) {
-                // Invalid password
-                setErrorMessages({ name: "pass", message: errors.pass });
-            } else {
-                setIsSubmitted(true);
-            }
-        } else {
-            // Username not found
-            setErrorMessages({ name: "uname", message: errors.uname });
-        }
-    };
-    // Generate JSX code for error message
-    const renderErrorMessage = (name) =>
-        name === errorMessages.name && (
-            <div className="error"><p>{errorMessages.message}</p></div>
-        );
-
-    const renderForm = (
-        <>
-            <div className="loginForm">
-            <Container>
-                <div className="screen">
-                    <div className="screen__content">
-                        <form className="login" onSubmit={handleSubmit}>
-                            <div className="login__field">
-                                <input name="uname" type="text" className="login__input" placeholder="User name / Email" />
-                                {renderErrorMessage("uname")}
-                            </div>
-                            <div className="login__field">
-                                <input name="pass" type="password" className="login__input" placeholder="Password" />
-                                {renderErrorMessage("pass")}
-                            </div>
-                            <button type="submit" className="button login__submit">
-                                <span className="button__text">Log In Now</span>
-                            </button>
-                        </form>
-  
-                    </div>
-                    <div className="screen__background">
-                        <span className="screen__background__shape screen__background__shape4"></span>
-                        <span className="screen__background__shape screen__background__shape3"></span>
-                        <span className="screen__background__shape screen__background__shape2"></span>
-                        <span className="screen__background__shape screen__background__shape1"></span>
-                    </div>
-                </div>
-            </Container>
-            </div>
-        </>
-
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">
+        <p>{errorMessages.message}</p>
+      </div>
     );
-    return (
-        <>
-            {isSubmitted ? <AddService/> : renderForm}
-        </>
-    )
-}
 
-export default Login
+  const renderForm = (
+    <>
+      <div className="adminLogin">
+      <div className="loginForm">
+        <div className="screen">
+          <div className="screen__content">
+            <form className="login" onSubmit={handleSubmit}>
+              <div className="login__field">
+                <input
+                  name="uname"
+                  type="text"
+                  className="login__input"
+                  placeholder="User name / Email"
+                />
+                {renderErrorMessage("uname")}
+              </div>
+              <div className="login__field">
+                <input
+                  name="pass"
+                  type="password"
+                  className="login__input"
+                  placeholder="Password"
+                />
+                {renderErrorMessage("pass")}
+              </div>
+              <button type="submit" className="button login__submit">
+                <span className="button__text">Log In Now</span>
+              </button>
+            </form>
+          </div>
+          <div className="screen__background">
+            <span className="screen__background__shape screen__background__shape4"></span>
+            <span className="screen__background__shape screen__background__shape3"></span>
+            <span className="screen__background__shape screen__background__shape2"></span>
+            <span className="screen__background__shape screen__background__shape1"></span>
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+  return <>{isSubmitted ? <AddService /> : <>{renderForm}</>}</>;
+};
+
+export default Login;
--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,9 +1,152 @@
-import React from 'react'
-
+import React, { useState,useEffect } from "react";
+import "./Signup.css";
+import { Modal } from "react-bootstrap";
+import { loginUser, mobileOtp, registerUser } from "../api/auth";
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [phone, setPhone] = useState(true);
+  const [otpNumber, setOtpNumber] = useState(false);
+  const [pass, setPass] = useState(false);
 
-export default Signup
+  const [mobile,setMobile]=useState("");
+  const [password,setPassword]=useState("");
+  const [userMobile,setUserMobile]=useState("");
+  const [userPassword,setUserPassword]=useState("");
+  const [otpCode,setOtpCode]=useState("");
+  const [otpValue,setOtpValue]=useState("");
+  const [otpWrong,setOtpWrong]=useState(false);
+
+  function generateRandomNumber() {
+    const min = 10000;
+    const max = 99999;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+  const numberSave = () => {
+    setPhone(false);
+    setOtpNumber(true);
+    const randomNumber = generateRandomNumber();
+    console.log(randomNumber);
+    setOtpCode(randomNumber)
+    // mobileOtp(mobile,randomNumber);
+  };
+  const otpSave = () => {
+    console.log(otpCode,otpValue);
+    if(String(otpCode) === otpValue){
+      setOtpNumber(false);
+      setPass(true);
+      setOtpWrong(false)
+    }
+    else{
+      setOtpWrong(true)
+    }
+    
+  };
+  const loginSave = () => {
+    registerUser(mobile,password)
+    setShow(false);
+    setPass(false);
+    setPhone(true);
+  };
+
+  const handleMobileChange = (event) => {
+    const value = event.target.value;
+    setMobile(value);
+  };
+  const handlePassChange = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+  };
+
+  const handleMobile = (event) => {
+    const value = event.target.value;
+    setUserMobile(value);
+  };
+  const handlePass = (event) => {
+    const value = event.target.value;
+    setUserPassword(value);
+  };
+  const handleOtp = (event) => {
+    const value = event.target.value;
+    setOtpValue(value);
+  };
+
+  const loginApi=()=>{
+    loginUser(userMobile,userPassword)
+  }
+  return (
+    <>
+      <section className="loginBox">
+        <div className="app">
+          <form>
+            <h1>Welcome</h1>
+            <div className="inputs">
+              <input type="text" onChange={handleMobile} name="" placeholder="Mobile number" />
+              <input type="password" onChange={handlePass} name="" placeholder="password" />
+              {/*
+            <p className="light">
+                <a href="#">Forgot password?</a>
+              </p>
+            */}
+            </div>
+          </form>
+
+          <div className="loginFooter">
+            <button onClick={loginApi}>Continue</button>
+            <p onClick={handleShow}>Create a account</p>
+          </div>
+        </div>
+      </section>
+
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>User Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="number">
+            {phone ? (
+              <>
+                <label id="mobile">Enter your mobile number</label> <br />
+                <input type="text" onChange={handleMobileChange} placeholder="Mobile number" /> <br />
+                <span>Example: 0178454512</span> <br />
+                <button className="btn" onClick={numberSave}>
+                  Continue
+                </button>
+              </>
+            ) : (
+              <></>
+            )}
+            {otpNumber ? (
+              <>
+                <label id="mobile">Enter your OTP number</label> <br />
+                <input type="text" onChange={handleOtp} placeholder="OTP number" /> <br />
+               {otpWrong ? <><span>This code is not correct</span></>:<></>} <br />
+                <button className="btn" onClick={otpSave}>
+                  Continue
+                </button>
+              </>
+            ) : (
+              <></>
+            )}
+            {pass ? (
+              <>
+                <label id="mobile">Enter your password</label> <br />
+                <input type="password" onChange={handlePassChange} placeholder="Password" /> <br />
+                <button className="btn" onClick={loginSave}>
+                  Save
+                </button>
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default Signup;
